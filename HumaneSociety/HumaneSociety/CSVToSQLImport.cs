cf483f86e2b5daf56c3d825814f15a906d240e16
Evan-Farr <evan.c.farr@gmail.com>
2017-05-01 08:25:47 -0500

    trouble with csv

--- HumaneSociety/HumaneSociety/CSVToSQLImport.cs
+++ HumaneSociety/HumaneSociety/CSVToSQLImport.cs
@@ -17,11 +17,28 @@ namespace HumaneSociety
 
         public static void ImportCSV(string file)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
             var imported = ReadCSV(file);
             foreach(var data in imported)
             {
-
+                 
             }
         }
+
+        //    SqlBulkCopy objBulk = new SqlBulkCopy(connection);
+        //    objBulk.DestinationTableName = "Animal";
+        //    objBulk.ColumnMappings.Add("Species", "Species");
+        //    objBulk.ColumnMappings.Add("Name", "Name");
+        //    objBulk.ColumnMappings.Add("Gender", "Gender");
+        //    objBulk.ColumnMappings.Add("Age", "Age");
+        //    objBulk.ColumnMappings.Add("Breed", "Breed");
+        //    objBulk.ColumnMappings.Add("Size", "Size");
+        //    objBulk.ColumnMappings.Add("Personality_Type", "Personality_Type");
+        //    objBulk.ColumnMappings.Add("Special_Needs", "Special_Needs");
+        //    objBulk.ColumnMappings.Add("Food_Requirements", "Food_Requirements");
+        //    objBulk.ColumnMappings.Add("Spayed_Or_Neutered", "Spayed_Or_Neutered");
+        //    objBulk.ColumnMappings.Add("Recieved_Shots", "Recieved_Shots");
+        //    objBulk.ColumnMappings.Add("Adoption_Price", "Adoption_Price");
+        //    objBulk.ColumnMappings.Add("Category", "Category");
     }
 }
--- HumaneSociety/HumaneSociety/Connection.cs
+++ HumaneSociety/HumaneSociety/Connection.cs
@@ -25,30 +25,6 @@ namespace HumaneSociety
             }
         }
 
-        //public static void InsertCSVRecords(DataTable csvdt)
-        //{
-        //    IDbConnection connection;
-        //    SqlBulkCopy objBulk = new SqlBulkCopy(connection);
-        //    objBulk.DestinationTableName = "Animal";
-        //    objBulk.ColumnMappings.Add("Species", "Species");
-        //    objBulk.ColumnMappings.Add("Name", "Name");
-        //    objBulk.ColumnMappings.Add("Gender", "Gender");
-        //    objBulk.ColumnMappings.Add("Age", "Age");
-        //    objBulk.ColumnMappings.Add("Breed", "Breed");
-        //    objBulk.ColumnMappings.Add("Size", "Size");
-        //    objBulk.ColumnMappings.Add("Personality_Type", "Personality_Type");
-        //    objBulk.ColumnMappings.Add("Special_Needs", "Special_Needs");
-        //    objBulk.ColumnMappings.Add("Food_Requirements", "Food_Requirements");
-        //    objBulk.ColumnMappings.Add("Spayed_Or_Neutered", "Spayed_Or_Neutered");
-        //    objBulk.ColumnMappings.Add("Recieved_Shots", "Recieved_Shots");
-        //    objBulk.ColumnMappings.Add("Adoption_Price", "Adoption_Price");
-        //    objBulk.ColumnMappings.Add("Category", "Category");
-        //    database.Open();
-        //    objBulk.WriteToServer(csvdt);
-        //    database.Close();
-
-        //}
-
         public static void SearchBySpecies()
         {
             HumaneSocietyDataContext database = new HumaneSocietyDataContext();
