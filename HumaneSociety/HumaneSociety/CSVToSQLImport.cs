e9a666f7f74248abcf089cbdf86c13ad27c8ad3e
Evan-Farr <evan.c.farr@gmail.com>
2017-05-01 08:13:50 -0500

    trying to figure out CSV

---
+++ HumaneSociety/HumaneSociety/CSVToSQLImport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class CSVToSQLImport
+    {
+        public static List<string[]> ReadCSV(string file)
+        {
+            var dataImport = File.ReadLines(file).Select(l => l.Split(',')).Select(x => x).ToList();
+            return dataImport;
+        }
+
+        public static void ImportCSV(string file)
+        {
+            var imported = ReadCSV(file);
+            foreach(var data in imported)
+            {
+
+            }
+        }
+    }
+}
